4f60f953f83b297194b872d06e52e14f56399fd4
AhmedMahmoudObeidHassani <s1023081@uomustansiriyah.edu.iq>
2024-11-28 12:13:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Diagnostics.Eventing.Reader;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using static System.Console;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Win32;
+namespace OOP_Progect_Library
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            WriteLine("will we see it?");
+
+
+
+
+
+
+        }
+
+    }
+}
